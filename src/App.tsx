e8851ceef27faee7ab4d5a1804b0891d9abf78ce
Vincent <vincentliu362@gmail.com>
2024-01-10 11:31:39 -0600

    refactor: remove logos

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,7 @@ import { Counter } from "./components/Counter";
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Counter />
     </>
   );
